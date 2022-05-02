a8cc9dc457c0a789161becdae33a3d2805b850ac
bobo5487 <100327431+bobo5487@users.noreply.github.com>
2022-05-02 17:55:18 +0800

    Update main.c

--- lab3/main.c
+++ lab3/main.c
@@ -1,33 +1,34 @@
-#include <stdio.h>
+#include<stdio.h>
 
-int my_strlen(char* s) {
-	int len = 0;
-	for(int i = 0; *(s + i) != '\0'; i++) {len++;}
-	return len;
+int my_strlen(char* s){
+	int n = 0;
+	while(*(s+n) != '\0')n++;
+	return n;
+} 
+
+int rec_strlen(char* s){
+	if (*s == '\0')return 0;
+	else return 1+rec_strlen(s+1);
 }
 
-int rec_strlen(char* s) {
-	if(*s == '\0') {return 0;}
-	else {return rec_strlen(s + 1) + 1;}
-}
-
-char* my_sort(char* s) {
-	int len = 0;
-	for(int i = 0; *(s + i) != '\0'; i++) {len++;}
-	for(int i = 0; i < len; i++) {
-		for(int j = 0; j < len - 1 - i; j++) {
-			if(*(s + j) > *(s + j + 1)) {char temp = *(s + j);
-							*(s + j) = *(s + j + 1);
-							*(s + j + 1) = temp;}
+char* my_sort(char* s){
+	int j, k, temp;
+	for(j=0; *(s+j) != '\0'; j++){
+		for(k=j; *(s+k) != '\0'; k++){
+			if (*(s+j) > *(s+k)){
+				temp = *(s+j);
+				*(s+j) = *(s+k);
+				*(s+k) = temp;
+			}
 		}
 	}
 	return s;
 }
 
-int main(void) {
+int main(){
 	char s[] = "IU is a girl!";
-	printf("len = %d\n", my_strlen(s));
-	printf("len = %d\n", rec_strlen(s));
-	printf("sort = %s\n", my_sort(s));
+	printf("len = %d", my_strlen(s));
+	printf("len = %d", rec_strlen(s));
+	printf("sort = %s", my_sort(s));
 	return 0;
 }
